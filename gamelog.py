3efbbda4e21b75ab36feda020a70d66e512b6c10
y8cui <cuiyuan51@gmail.com>
2016-06-14 23:53:01 -0700

    add gamelog data

--- gamelog.py
+++ gamelog.py
@@ -1,10 +1,11 @@
 import requests
-import sqlite3
-
-conn = sqlite3.connect('nba.db')
-c = conn.cursor()
 
 #http://stats.nba.com/stats/leaguegamelog?Counter=1000&Direction=DESC&LeagueID=00&PlayerOrTeam=P&Season=2015-16&SeasonType=Regular+Season&Sorter=PTS
+HEADERS = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/45.0.2454.101 Safari/537.36'),
+           'referer': 'http://stats.nba.com/scores/'
+          }
 
 gamelog_base_url = 'http://stats.nba.com/stats/leaguegamelog'
 league_id = '00' #nba league id
@@ -21,7 +22,13 @@ params = {'Counter': 1000,
           'Sorter': 'PTS'
          }
 
-response = requests.get( gamelog_base_url, params = params )
+response = requests.get( gamelog_base_url, params = params, headers = HEADERS )
 response.raise_for_status() # raise exception if invalid response
+gamelogs = response.json()['resultSets'][0]['rowSet']
+
+f = open("gamelogs.txt", "w")
+for log in gamelogs:
+  f.write("%s\n" % log)
+f.close()
 
 
--- player.py
+++ player.py
@@ -1,5 +1,10 @@
 import requests
 
+HEADERS = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/45.0.2454.101 Safari/537.36'),
+           'referer': 'http://stats.nba.com/scores/'
+          }
 #http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=1&LeagueID=00&Season=2015-16
 
 player_base_url = 'http://stats.nba.com/stats/commonallplayers'
@@ -7,11 +12,11 @@ league_id = '00' #nba league id
 season = '2015-16' #nba season
 
 params = {'IsOnlyCurrentSeason': 1, 'LeagueID': league_id, 'Season': season}
-response = requests.get( player_base_url, params = params )
+response = requests.get( player_base_url, params = params, headers = HEADERS )
 response.raise_for_status() # raise exception if invalid response
 players = response.json()['resultSets'][0]['rowSet']
 
-f = open("test.txt", "w")
+f = open("players.txt", "w")
 for player in players:
   f.write("%s\n" % player)
 f.close()
